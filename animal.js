21436b800fc9398ad0f1af1b2d4fbd105cb8db57
pietroaulas <143614391+pietrofofo@users.noreply.github.com>
2026-06-08 13:49:06 +0000

    versao1

--- animal.js
+++ animal.js
@@ -1,1 +1,16 @@
+const animal = {
+    nome: "Thor", //string
+    especie: "Cachorro",
+    idade: 4, //number
+    tutor: false, //boolean
+    vacinado: true
+};
 
+//console.log(animal);
+console.log(animal.nome);
+console.log(animal.tutor);
+
+animal.idade = 6;
+//console.log(animal);
+
+console.log(`${animal.nome} é um ${animal.especie} com a idade ${animal.idade} e possui sua vacina em dia (${animal.vacinado}).`);
